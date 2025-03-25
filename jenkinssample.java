cb115b1516c36eba157e71ae192c3943dc3e579d
arekela-navya-bharathi <245122749003@mvsrec.edu.in>
2025-03-25 09:53:31 +0000

    Update jenkinssample.java

--- jenkinssample.java
+++ jenkinssample.java
@@ -4,10 +4,8 @@ class jenkinssample
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter a number");
-		int n1 = sc.nextInt();
-		System.out.println("enter next number");
-		int n2 = sc.nextInt();
+		int n1=10,n2=5;
+		
 		System.out.println("addition"+(n1+n2)+"substraction"+(n1-n2)+"multiplication"+(n1*n2));
 	}
 }
